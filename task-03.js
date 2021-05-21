22cf6075d1ffa6ebf0730350f52fbc8c12465e93
gigorko1993 <gigorko1993@gmail.com>
2021-05-21 23:47:05 +0300

    made formating by prettier

--- task-03.js
+++ task-03.js
@@ -2,19 +2,19 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          const canProcess = Math.random() > 0.3;
-      
-          if (canProcess) {
-            resolve(transaction.id, delay);
-          } else {
-            reject(transaction.id);
-          }
-        }, delay);
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve(transaction.id, delay);
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
 };
 
 const logSuccess = (id, time) => {
@@ -28,18 +28,10 @@ const logError = id => {
 /*
  * Должно работать так
  */
-makeTransaction({ id: 70, amount: 150 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
-  .then(logSuccess)
-  .catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
